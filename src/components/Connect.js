ad6b9e257fbe5b6a7f7d4362f415dc5699f767ab
Kaushal Karki <kaushal.karki@maropost.com>
2024-07-31 09:12:30 +0530

    connect page changes

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import '../styles/Connect.css';
+import { color } from 'chart.js/helpers';
 
 const Connect = () => {
   return (
     <div className="connect-container">
       <h1 className="connect-container__header">Connect with Me</h1>
       <div className="connect-container__social-links">
-        <a href="https://wa.me/+918474905343" className="connect-container__social-link" target="_blank" rel="noreferrer">
-          <i className="zmdi zmdi-whatsapp"></i> WhatsApp
+        <a href="https://wa.me/+918474905343" className="connect-container__social-link" style={{ color: "green" }} target="_blank" rel="noreferrer">
+          <i className="zmdi zmdi-whatsapp mdc-text-green"></i> WhatsApp
         </a>
-        <a href="https://www.facebook.com/kaushalthelion" className="connect-container__social-link" target="_blank" rel="noreferrer">
+        <a href="https://www.facebook.com/kaushalthelion" className="connect-container__social-link" style={{ color: "#0866ff" }}   target="_blank" rel="noreferrer">
           <i className="zmdi zmdi-facebook"></i> Facebook
         </a>
-        <a href="https://www.linkedin.com/in/kaushalkarki" className="connect-container__social-link" target="_blank" rel="noreferrer">
-          <i className="zmdi zmdi-linkedin"></i> LinkedIn
-        </a>
-        <a href="https://github.com/kaushalkarki" className="connect-container__social-link" target="_blank" rel="noreferrer">
+        <a href="https://github.com/kaushalkarki" className="connect-container__social-link" style={{ color: "black" }}target="_blank" rel="noreferrer">
           <i className="zmdi zmdi-github"></i> GitHub
         </a>
-        <a href="mailto:kaushalsinghkarki@gmail.com" className="connect-container__social-link" target="_blank" rel="noreferrer">
+        <a href="mailto:kaushalsinghkarki@gmail.com" className="connect-container__social-link" style={{ color: "red" }}target="_blank" rel="noreferrer">
           <i className="zmdi zmdi-email"></i> Gmail
         </a>
-        <a href="https://www.instagram.com/kaushalsinghkarki" className="connect-container__social-link" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/kaushalkarki" className="connect-container__social-link" style={{color: "#0a66c2"}}target="_blank" rel="noreferrer">
+          <i className="zmdi zmdi-linkedin"></i> LinkedIn
+        </a>
+        <a href="https://www.instagram.com/kaushalsinghkarki" className="connect-container__social-link" style={{ color: "purple" }}target="_blank" rel="noreferrer">
           <i className="zmdi zmdi-instagram"></i> Instagram
         </a>
       </div>
